19b51ad553d20aa146048f111ccd8ce0d30dd5c0
Bala C <balasl342@gmail.com>
2024-11-27 12:32:36 +0530

    Add JWT token validation

--- internal/adapters/auth/jwt.go
+++ internal/adapters/auth/jwt.go
@@ -25,3 +25,14 @@ func (j *JWTService) GenerateToken(userID string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(j.SecretKey))
 }
+
+func (j *JWTService) ValidateToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(j.SecretKey), nil
+	})
+	if err != nil {
+		log.Println("Invalid token")
+		return nil, err
+	}
+	return token, nil
+}
