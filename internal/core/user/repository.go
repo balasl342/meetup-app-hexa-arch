f54b1b7f7f5dc46bda04c14fc43a46d11900a129
Bala C <balasl342@gmail.com>
2024-12-13 13:56:50 +0530

    DBupdate for signup and sign in

--- internal/core/user/repository.go
+++ internal/core/user/repository.go
@@ -1,6 +1,9 @@
 package user
 
 import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -13,4 +16,27 @@ type MongoDBUserRepository struct {
 func NewMongoDBUserRepository(client *mongo.Client, dbName, collectionName string) *MongoDBUserRepository {
 	collection := client.Database(dbName).Collection(collectionName)
 	return &MongoDBUserRepository{collection: collection}
+}
+
+// Save saves a new user to MongoDB.
+func (r *MongoDBUserRepository) Save(ctx context.Context, user *User) error {
+	_, err := r.collection.InsertOne(ctx, user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetByEmail retrieves a user from MongoDB by their email.
+func (r *MongoDBUserRepository) GetByEmail(ctx context.Context, email string) (*User, error) {
+	var user User
+	filter := bson.D{{Key: "email", Value: email}}
+	err := r.collection.FindOne(ctx, filter).Decode(&user)
+	if err == mongo.ErrNoDocuments {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
